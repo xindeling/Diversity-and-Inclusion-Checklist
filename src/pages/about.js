6eb151d3c2cb5f27ec48d95487c66e60b20de355
Xindeling Pan <xindeling@gmail.com>
2020-12-04 02:35:06 -0500

    add page title

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,16 @@
 import React from "react"
 //import { Link } from "gatsby"
+import { Helmet } from "react-helmet"
 import Header from "../components/header"
 
 export default function About() {
   return (
     <div style={{ color: `teal` }}>
+      <Helmet>
+        <title>About</title>
+        <meta name="description" content="A hands-on diversity and inclusion checklist for designers" />
+        <meta name="author" content="Xindeling Pan" />
+      </Helmet>
       <Header headerText="About"/>
       <p><i>currently under construction</i></p>
     </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 //import { Link } from "gatsby"
+import { Helmet } from "react-helmet"
 import Header from "../components/header"
 
 
@@ -7,6 +8,11 @@ import Header from "../components/header"
 export default function Home() {
   return (
     <div>
+      <Helmet>
+        <title>Diversity and Inclusion Checklist</title>
+        <meta name="description" content="A hands-on diversity and inclusion checklist for designers" />
+        <meta name="author" content="Xindeling Pan" />
+      </Helmet>
       <Header headerText="Diversity & Inclusion Checklist"/>
       <p><i>currently under construction</i></p>
       <p>Please visit the work-in-progress <a className="link" id="checklistLink" rel="external noreferrer noopener" target="_blank" href="https://www.notion.so/A-Diversity-Inclusion-Checklist-c4e2d818d55849df88cf0a3e5863671d"><b>checklist</b></a> on Notion</p>
